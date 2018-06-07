f26a6b43749fa5256524ad839dc50529fa8bf090
unknown <subeen2150@gmail.com>
2018-06-07 09:56:33 +0900

    default constructor added

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -1,7 +1,11 @@
 public class MyMessage {
 	String message;
+
 	public MyMessage(String message) {
 		this.message = message;
+	}
+	public void print() {
+		System.out.println(message);
 	}
 	public void print() {
 		System.out.println(message);
