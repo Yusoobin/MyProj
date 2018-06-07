8512b51665fe10d39822106df4c8a89215e539b4
unknown <subeen2150@gmail.com>
2018-06-07 10:03:00 +0900

    defulat constructor modified

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -1,9 +1,10 @@
 public class MyMessage {
 	String message;
 
-	public MyMessage(String message) {
-		this.message = message;
+	public setMyMessage(String message) {
+		this.message = no;
 	}
+
 	public void print() {
 		System.out.println(message);
 	}
